46e796a57aec0afe77ebd31ba7ba9ab91821fe42
microQZR <zi.han.meng100@gmail.com>
2021-09-05 15:45:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Reusable React Components Library
+
+A collection of React components that could be used for various projects.
+
+The components in this library use CSS Modules and inline styles for styling
+
+Copyright &copy; 2021 MicroQZR
---
+++ components/Modal.js
@@ -0,0 +1,13 @@
+import classes from "./Modal.module.css";
+
+const Modal = props => {
+  return (
+    <div className={`flex ${classes.bg}`} onClick={props.closeHandler}>
+      <div className="flex-column" onClick={e => e.stopPropagation()}>
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
